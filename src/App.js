858741e275d1926382d7b28983d0d2a3cf7a92cb
martagrio <marta.igr90@gmail.com>
2019-09-23 09:44:50 +0200

    Paint characters in main

--- src/App.js
+++ src/App.js
@@ -26,12 +26,26 @@ class App extends React.Component {
 	}
 
   render() {
+		const {characters} = this.state;
     return (
       <div className="app">
 				<header className="app__header">
 					<h1 className="app__title">rick and morty</h1>
 				</header>
 				<main className="app__main">
+					<ul className="characters__list">
+						{characters.map( chac => {
+							return (
+								<li className="character" key={chac.id}>
+									<div className="character__img">
+										<img src={chac.image} alt={chac.name}/>
+									</div>
+									<h2 className="chacracter__name">{chac.name}</h2>
+									<h3 className="character__species">{chac.species}</h3>
+								</li>
+							);
+						})}
+					</ul>
 				</main>
       </div>
     );
