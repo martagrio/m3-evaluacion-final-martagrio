414f30b82397989fbeb5bcfd4d4b2b772cc99918
martagrio <marta.igr90@gmail.com>
2019-09-23 09:11:56 +0200

    Add title and image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import './css/Header.css'
 
-function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+		this.state = {
+			characters: []
+		}
+  }
+
+  render() {
+    return (
+      <div className="app">
+				<header className="app__header">
+					<h1 className="app__title">rick and morty</h1>
+				</header>
+      </div>
+    );
+  }
 }
 
 export default App;
