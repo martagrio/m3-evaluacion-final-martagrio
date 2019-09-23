559042249b07939734d2398019b42fe4986da8ad
martagrio <marta.igr90@gmail.com>
2019-09-23 09:59:44 +0200

    Create filter function and Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {fetchCharacters} from './services/characters';
 import './App.css';
+import Header from './components/Header';
 import './css/Header.css'
 
 class App extends React.Component {
@@ -8,8 +9,11 @@ class App extends React.Component {
     super(props);
 
 		this.state = {
-			characters: []
+			characters: [],
+			query: ''
 		}
+
+		this.getQuery = this.getQuery.bind(this);
   }
 
 	componentDidMount() {
@@ -25,25 +29,40 @@ class App extends React.Component {
 		})
 	}
 
+	getQuery(event){
+		const userQuery = event.currentTarget.value;
+		this.setState({
+			query: userQuery
+		})
+	}
+
   render() {
-		const {characters} = this.state;
+		const {characters, query} = this.state;
     return (
       <div className="app">
-				<header className="app__header">
-					<h1 className="app__title">rick and morty</h1>
-				</header>
+				<Header />
+
 				<main className="app__main">
+					<div className="app__search">
+						<input className="app__input" 
+							type="text" 
+							onChange = {this.getQuery} 
+							value={query}
+							placeholder = "Please, enter the name of the desired character" />	
+					</div>
 					<ul className="characters__list">
-						{characters.map( chac => {
-							return (
-								<li className="character" key={chac.id}>
-									<div className="character__img">
-										<img src={chac.image} alt={chac.name}/>
-									</div>
-									<h2 className="chacracter__name">{chac.name}</h2>
-									<h3 className="character__species">{chac.species}</h3>
-								</li>
-							);
+						{characters
+							.filter(searched => searched.name.toLowerCase().includes(query.toLowerCase()))
+							.map( chac => {
+								return (
+									<li className="character" key={chac.id}>
+										<div className="character__img">
+											<img src={chac.image} alt={chac.name}/>
+										</div>
+										<h2 className="chacracter__name">{chac.name}</h2>
+										<h3 className="character__species">{chac.species}</h3>
+									</li>
+								);
 						})}
 					</ul>
 				</main>
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = props => {
+	return(
+		<header className="app__header">
+			<h1 className="app__title">rick and morty</h1>
+		</header>
+	);
+}
+export default Header;
